e7f24b59d12ccf944596598fcbbf243dd6f2da45
tokiw <masatokiwa6373@gmail.com>
2014-07-15 20:19:22 +0900

    add ex23,24

---
+++ java_practice/ch24/ex24_1/GlobalRes_en.java
@@ -0,0 +1,16 @@
+package ex24_1;
+
+import java.util.ListResourceBundle;
+
+public class GlobalRes_en extends ListResourceBundle{
+
+    @Override
+	protected Object[][] getContents() {
+		return contents;
+	}
+
+	private static final Object[][] contents = {
+		{GlobalRes.HELLO, "Hello"},
+		{GlobalRes.GOODBYE, "Goodbye"},
+	};
+}
