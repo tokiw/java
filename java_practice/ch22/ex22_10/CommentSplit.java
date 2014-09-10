097e93d3e279fcb6388b9abdac2cbc10855fac4c
tokiw <masatokiwa6373@gmail.com>
2014-09-11 00:10:20 +0900

    add ex22_10

---
+++ java_practice/ch22/ex22_10/CommentSplit.java
@@ -0,0 +1,44 @@
+package ex22_10;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class CommentSplit {
+	public static List<String> split(Readable source) throws IOException {
+		Scanner in = new Scanner(source);
+		
+		List<String> vals = new ArrayList<>();
+		
+		in.useDelimiter(in.delimiter().toString() + "|#.*");
+		while (in.hasNext()) {
+			String line = in.next();
+			if (!line.equals("")) {
+				vals.add(line);
+			}
+		}
+		in.close();
+		
+		return vals;
+	}
+	
+	
+	public static void main(String[] args) {
+		try {
+			FileReader file = new FileReader("./ch22/ex22_10/sample.txt");
+
+			List<String> list = split(file);
+			Iterator<String> i = list.iterator();
+			while (i.hasNext()) { 
+				System.out.println((String)i.next());
+			}
+		} catch (IOException e) {
+			// TODO é©ìÆê∂ê¨Ç≥ÇÍÇΩ catch ÉuÉçÉbÉN
+			e.printStackTrace();
+		}
+	}
+}
